6d132ec6ede98f6da8f9ba006074ec0ca7b1a0e3
Sierra Lopez <sierralopez1006@gmail.com>
2022-09-29 19:35:55 -0700

    tried code

--- public/index.js
+++ public/index.js
@@ -3,6 +3,12 @@ function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
+    //async function getJSON(){
+       // let response = await fetch('http://192.168.1.236:9001/counter')
+       // let responseJSON = await response.json()
+        //console.log(responseJSON)
+   // }
+
     let countValue = 0;
 
     function increment(){
